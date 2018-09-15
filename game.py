ecccf6299ed886ec252832c0b241a7a6c265d9a2
Allison Ines <allisonshaye@gmail.com>
2018-09-14 17:21:15 -0700

    Add logic to select index of nested array to play

--- game.py
+++ game.py
@@ -3,15 +3,10 @@ from tictactoe import *
 board = create_board()
 
 
-# for row in board:
-  # print(row)
-
 board_dictionary = row_definitions(board)
-# print(board_dict['left_col'])
-# for i,j in board_dict.items():
-#   print "%s ---- %s" %(i, j)
-results_o = audit_board(board_dictionary, 'o')
-results_x = audit_board(board_dictionary, 'x')
+
+results_o = check_board(board_dictionary, 'o')
+results_x = check_board(board_dictionary, 'x')
 
 # for i,j in results_x.items():
 #   print "%s ===== %s" %(i,j)
@@ -20,13 +15,9 @@ results_x = audit_board(board_dictionary, 'x')
 # print(results_x)
 # print(game_over(results_o))
 # print(game_over(results_x))
-nextt = determine_next_move(board_dictionary, results_o, results_x)
-print(nextt)
+next_row_data = determine_next_row(board_dictionary, results_x, results_o)
+# print(next_row)
+row_to_box_dict = row_to_box_defs()
+determine_next_box(row_to_box_dict, board, next_row_data, 'o')
 
-
-  # board = [
-  #   ['x', 'o', 'x'],
-  #   ['x', 'o', '-'],
-  #   ['-', 'x', 'x']
-  # ]
 
--- tictactoe.py
+++ tictactoe.py
@@ -3,13 +3,13 @@ import random
 
 def create_board():
   board = [
-    ['o', 'x', '-'],
-    ['-', 'x', '-'],
-    ['-', '-', '-']
+    ['x', 'x', 'o'],
+    ['x', 'o', '-'],
+    ['o', '-', '-']
   ]
   return board
 
-
+# GOAL IS TO GET RID OF BOARD_DICT AND JUST USE ROW_TO_BOX_DEFS BISHH
 def row_definitions(board):
   board_dict = {
     'top_row': board[0],
@@ -24,13 +24,23 @@ def row_definitions(board):
   return board_dict
 
 
-def audit_board(board_dict, tile):
-  results = { 
-    3: [], 
-    2: [], 
-    1: [], 
-    0: [] 
+def row_to_box_defs():
+  box_dict = {
+    'top_row': [[0,0], [0,1], [0,2]],
+    'mid_row': [[1,0], [1,1], [1,2]],
+    'bottom_row': [[2,0], [2,1], [2,2]],
+    'left_col': [[0,0], [1,0], [2,0]],
+    'mid_col': [[0,1], [1,1], [2,1]],
+    'right_col': [[0,2], [1,2], [2,2]],
+    'diag_top_left_down': [[0,0], [1,1], [2,2]],
+    'diag_bot_left_up': [[2,0], [1,1], [0,2]]
   }
+
+  return box_dict
+
+
+def check_board(board_dict, tile):
+  results = { 3: [], 2: [], 1: [], 0: [] }
 
   for row_name, row_contents in board_dict.items():
     tile_count = row_contents.count(tile)
@@ -49,8 +59,8 @@ def audit_board(board_dict, tile):
 def game_over(results_dict):
   return True if results_dict[3] else False
 
-# return row
-def move_on_twos(board_dict, row_list):
+
+def move_on_twos_row_choice(board_dict, row_list):
   next_move = ''
   for row in row_list:
     row_contents = board_dict[row]
@@ -59,44 +69,65 @@ def move_on_twos(board_dict, row_list):
       break
   return next_move
 
-def first_move(row_list):
-  return next_move = random.choice(row_list)
 
-
-# needs refactoring; returns ROW to play
-def determine_next_row(board_dict, self_results, opponent_results):
-  move_found = False
-  next_move = ''
-
-  if self_results[2]:
-    next_move = move_on_twos(board_dict, self_results[2])
-    if next_move != '':
-      move_found = True
-
-  if opponent_results[2] and not move_found:
-    next_move = move_on_twos(board_dict, opponent_results[2])
-    if next_move != '':
-      move_found = True
-
-  if not move_found:
-    if self_results[1]:
-      for row in self_results[1]:
-        if row.count('-') == 2:
-          move_found = True
-          next_move = row
-          break
-
-  if not move_found:
-    if self_results[0]:
-      next_move = first_move(self_results[0])
-      if next_move != '':
-        move_found = True # don't think i need this since it's the last check.
-
+def random_row_choice(row_list):
+  next_move = random.choice(row_list)
   return next_move
 
 
-def determine_next_spot(board_dict, move):
-  pass
+def determine_next_row(board_dict, self_results, opponent_results):
+  row_found = False
+  next_row = ''
+
+  if self_results[2]: # win game
+    next_row = move_on_twos_row_choice(board_dict, self_results[2])
+    available_spots = 1
+    if next_row != '': row_found = True
+
+  if opponent_results[2] and not row_found: # prevent opponent from winning
+    next_row = move_on_twos_row_choice(board_dict, opponent_results[2])
+    available_spots = 1
+    if next_row != '': row_found = True
+
+  if not row_found: # make 2 in a row
+    if self_results[1]:
+      for row in self_results[1]:
+        row_contents = board_dict[row]
+        if row_contents.count('-') == 2:
+          row_found = True
+          next_row = row
+          available_spots = 2
+          break
+
+  if not row_found: # play first move
+    avail_rows = self_results[1] + self_results[0]
+    next_row = random_row_choice(self_results[0])
+    available_spots = 3
+
+  return [next_row, available_spots]
+
+
+def determine_next_box(row_box_dict, board, row_data, tile):
+  row_boxes = row_box_dict[row_data[0]]
+  available_spots = row_data[1] 
+
+  if available_spots == 1: 
+    for box in row_boxes:
+      if board[box[0]][box[1]] == '-':
+        next_box = box
+  
+  if available_spots == 2:
+    avail_spots = []
+    for box in row_boxes: #indices
+      if board[box[0]][box[1]] == '-':
+        avail_spots.append(box)
+    next_box = random.choice(avail_spots)
+
+  if available_spots == 3:
+    next_box = random.choice(row_boxes)
+
+  print(next_box)
+  return next_box
 
 
 
