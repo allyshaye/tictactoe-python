14a18b3cc193083fb92cdd3d525325a708eb7a9d
Allison Ines <allisonshaye@gmail.com>
2018-09-24 20:08:02 -0700

    add logic to game

--- game.py
+++ game.py
@@ -1,11 +1,30 @@
 from tictactoe import *
 
 board = create_board()
+bd = board_dictionary(board)
 
-board_dictionary = board_dictionary(board)
+current_tile = 'x'
+opponent_tile = 'o'
 
-results_o = check_board(board_dictionary, 'o')
-results_x = check_board(board_dictionary, 'x')
-next_row_data = determine_next_row(board_dictionary, results_x, results_o)
-print(determine_next_box(board_dictionary, board, next_row_data))
+game_over = False
 
+while not game_over:
+  board = make_move(current_tile, opponent_tile, bd, board)
+  bd = board_dictionary(board)
+
+  print_board(board)  
+  
+  results = check_board(bd, current_tile)
+
+  if full_board(board) or results[3]:
+    game_over = True
+    print("Game over!")
+
+    if results[3]:
+      print "Winner = %s, Loser = %s" %(current_tile, opponent_tile)
+    else:
+      print("It's a tie!")
+
+  placeholder = current_tile
+  current_tile = opponent_tile
+  opponent_tile = placeholder
--- tictactoe.py
+++ tictactoe.py
@@ -3,14 +3,14 @@ import random
 
 def create_board():
   board = [
-    ['x', 'x', '-'],
-    ['x', 'o', '-'],
-    ['o', '-', '-']
+    ['-', '-', '-'],
+    ['-', '-', '-'],
+    ['-', '-', '-']
   ]
   return board
 
 
-def board_dictionary(board): #test
+def board_dictionary(board): 
   board_dict = {
     'top_row': [[[0,0], [0,1], [0,2]], board[0]],
     'mid_row': [[[1,0], [1,1], [1,2]], board[1]],
@@ -25,11 +25,11 @@ def board_dictionary(board): #test
   return board_dict
 
 
-def check_board(board_dict, tile): #test
+def check_board(board_dict, tile):
   results = { 3: [], 2: [], 1: [], 0: [] }
 
   for row_name, row_contents in board_dict.items():
-    tile_count = row_contents[1].count(tile) #added index
+    tile_count = row_contents[1].count(tile)
     if tile_count == 3:
       results[3].append(row_name)
     elif tile_count == 2:
@@ -38,17 +38,30 @@ def check_board(board_dict, tile): #test
       results [1].append(row_name)
     elif tile_count == 0:
       results[0].append(row_name)
+  
   return results
 
 
-def game_over(results_dict):
-  return True if results_dict[3] else False
+def full_board(board):
+  full = True
+  for row in board:
+    if '-' in row:
+      full = False
+      break
+  return full
 
+
+def find_row_with_empty_box(board_dict):
+  for row, row_contents in board_dict.items():
+    if '-' in row_contents[1]:
+      next_row = row
+      break
+  return next_row
 
 def move_on_twos_row_choice(board_dict, row_list):
   next_move = ''
   for row in row_list:
-    row_contents = board_dict[row][1] #added index
+    row_contents = board_dict[row][1] 
     if '-' in row_contents:
       next_move = row
       break
@@ -64,12 +77,12 @@ def determine_next_row(board_dict, self_results, opponent_results):
   row_found = False
   next_row = ''
 
-  if self_results[2]: # win game
+  if self_results[2]: 
     next_row = move_on_twos_row_choice(board_dict, self_results[2])
     available_spots = 1
     if next_row != '': row_found = True
 
-  if opponent_results[2] and not row_found: # prevent opponent from winning
+  if opponent_results[2] and not row_found: 
     next_row = move_on_twos_row_choice(board_dict, opponent_results[2])
     available_spots = 1
     if next_row != '': row_found = True
@@ -77,7 +90,7 @@ def determine_next_row(board_dict, self_results, opponent_results):
   if not row_found:
     if self_results[1]:
       for row in self_results[1]:
-        row_contents = board_dict[row][1] #added index
+        row_contents = board_dict[row][1] 
         if row_contents.count('-') == 2:
           row_found = True
           next_row = row
@@ -88,11 +101,13 @@ def determine_next_row(board_dict, self_results, opponent_results):
     if self_results[0]:
       next_row = random_row_choice(self_results[0])
       available_spots = 3
-
+    else:
+      next_row = find_row_with_empty_box(board_dict)
+      available_spots = 1
   return [next_row, available_spots]
 
 
-def determine_next_box(board_dict, board, row_data): #test
+def determine_next_box(board_dict, board, row_data): 
   row_boxes = board_dict[row_data[0]][0]
   available_spots = row_data[1] 
 
@@ -103,7 +118,7 @@ def determine_next_box(board_dict, board, row_data): #test
   
   if available_spots == 2:
     avail_spots = []
-    for box in row_boxes: #indices
+    for box in row_boxes: 
       if board[box[0]][box[1]] == '-':
         avail_spots.append(box)
     next_box = random.choice(avail_spots)
@@ -113,3 +128,18 @@ def determine_next_box(board_dict, board, row_data): #test
 
   return next_box
 
+
+
+def make_move(self_tile, opponent_tile, board_dict, board):
+  opponent_results = check_board(board_dict, opponent_tile)
+  self_results = check_board(board_dict, self_tile)
+  next_row = determine_next_row(board_dict, self_results, opponent_results)
+  next_spot = determine_next_box(board_dict, board, next_row)
+  board[next_spot[0]][next_spot[1]] = self_tile
+  return board
+
+
+def print_board(board):
+  print("\n")
+  for row in board:
+    print(row)
